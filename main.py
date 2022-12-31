e659d4e5491c5577443093afa882358639f489a2
rayankhaled26 <rk217230@gmail.com>
2022-12-31 11:40:48 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#second_dict2
+# Delete a list of keys from a dictionary
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"
+}
+sample_dict.pop("name")
+sample_dict.pop("salary")
+print(sample_dict)
